83eb968516401863579a37c4921b35bfe7d2aa83
Draculk <kayranvieira456@hotmail.com>
2024-02-03 19:29:22 -0300

    :sparkles: adding some new fields

--- 3 - imdb/main.py
+++ 3 - imdb/main.py
@@ -22,23 +22,45 @@ li = ul.findAll("li", {"class": "ipc-metadata-list-summary-item"})
 names = []
 years = []
 rates = []
+durations = []
+main_genre = []
 
 for tag in li:
-    # name
-    name = tag.find("h3", {"class": "ipc-title__text"})
-    names.append(name.text.split(".")[1][1:])
     # year
     year = tag.find(
         "span",
-        {"class": "sc-1e00898e-8 hsHAHC cli-title-metadata-item"}
+        {"class": "sc-be6f1408-8 fcCUPU cli-title-metadata-item"}
     )
     years.append(year.text)
+
     # rating
     rate = tag.find("span", {"class": "ipc-rating-star"})
     rates.append(rate.text[:3])
 
+    # duration
+    duration = tag.find(
+        "div",
+        {"class": "sc-be6f1408-7 iUtHEN cli-title-metadata"}
+    )
+    durations.append(f"{duration.text[4:].split('m')[0]}m")
+
+    link = tag.find("a", {"class": "ipc-title-link-wrapper"})
+    movie_url = f"https://www.imdb.com{link['href']}"
+
+    movie_r = requests.get(movie_url, headers=headers)
+    movie_html = movie_r.text
+    movie_soup = BeautifulSoup(movie_html, "html.parser")
+
+    # name
+    name = movie_soup.find("span", {"class": "hero__primary-text"})
+    names.append(name.text)
+
+    # genre
+    main_genre = movie_soup.find("span", {"class": "ipc-chip__text"})
+    main_genre.append(main_genre.text)
+
 with open('imdb.csv', "w", encoding='UTF-8') as f:
-    field_names = ['name', 'year', 'rate']
+    field_names = ['name', 'year', 'rate', 'duration', 'main_genre']
     imdb_writer = csv.DictWriter(f, delimiter=';',
                                  fieldnames=field_names,
                                  lineterminator='\n')
@@ -47,5 +69,7 @@ with open('imdb.csv', "w", encoding='UTF-8') as f:
         imdb_writer.writerow({
             "name": names[i],
             "year": years[i],
-            "rate": rates[i]
+            "rate": rates[i],
+            "duration": durations[i],
+            "main_genre": main_genre[i]
         })
