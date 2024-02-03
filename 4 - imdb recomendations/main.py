4930eb7ba95112839ece0e30681722b9b5c7e248
Draculk <kayranvieira456@hotmail.com>
2024-02-03 20:46:10 -0300

    :sparkles: recomendation system using top 250 movies

---
+++ 4 - imdb recomendations/main.py
@@ -0,0 +1,40 @@
+import requests
+from bs4 import BeautifulSoup
+
+movieName = input("Coloque o nome de um filme: ").lower()
+
+url = "https://www.imdb.com/chart/top/"
+headers = {
+    'User-Agent': ("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) "
+                   "AppleWebKit/537.36 (KHTML,like Gecko) Chrome/50.0.2661.102"
+                   " Safari/537.36")
+    }
+
+res = requests.get(url, headers=headers)
+html_content = res.text
+soup = BeautifulSoup(html_content, 'html.parser')
+
+# main ul with all data
+ul = soup.find("ul", {"class": "ipc-metadata-list"})
+# li with all movie divs
+li = ul.findAll("li", {"class": "ipc-metadata-list-summary-item"})
+
+for tag in li:
+    link = tag.find("a", {"class": "ipc-title-link-wrapper"})
+    movie_url = f"https://www.imdb.com{link['href']}"
+
+    movie_r = requests.get(movie_url, headers=headers)
+    movie_html = movie_r.text
+    movie_soup = BeautifulSoup(movie_html, "html.parser")
+
+    # name
+    name = movie_soup.find(
+        "span", {"class": "hero__primary-text"}
+        ).text
+
+    if movieName == name.lower():
+        print(f"Se gostou de {name}, também pode gostar de: ")
+        titles = movie_soup.findAll("span", {"data-testid": "title"})
+        for title in titles:
+            print(title.text)
+        break
